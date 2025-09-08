8cb5e83d8206b18a333c858823a355101b31fe36
Carlos Eduardo Mera Ruiz <carlosmeraruiz@gmail.com>
2025-09-08 17:14:54 -0500

    feat(nvim) Personalizado dashboard-nvim

--- nvim/.config/nvim/lua/core/plugins/dashboard-nvim.lua
+++ nvim/.config/nvim/lua/core/plugins/dashboard-nvim.lua
@@ -18,6 +18,44 @@ return {
             },
             week_header = {
                 enable = true,
+                append = {
+                    "Neovim: " .. vim.version().major .. "." .. vim.version().minor
+                }
+            },
+            shortcut = {
+                {
+                    desc = ' Lazy',
+                    group = 'DiagnosticHint',
+                    action = 'Lazy',
+                    key = 'l'
+                },
+                {
+                    desc = ' Mason Update',
+                    group = 'DiagnosticHint',
+                    action = 'Mason',
+                    key = 'm'
+                },
+                {
+                    desc = " Files",
+                    group = "file",
+                    action = "Number",
+                    key = "f",
+                },
+                {
+                    desc = ' Search Files',
+                    group = 'Label',
+                    action = 'Telescope find_files',
+                    key = 's',
+                },
+            },
+            project = {
+                enable = false,
+            },
+            mru = {
+                enable = false,
+            },
+            footer = {
+                'Krlos'
             }
         }
     }
