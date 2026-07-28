c4a31af8d91e5d0ce308d189a5d96503056959df
Carlos Eduardo Mera Ruiz <carlosemr24@yahoo.com.ar>
2026-07-28 12:07:40 -0500

    feat(nvim): Actualizado dashboard
    Se ajustan los atajos de teclado a personalizado, para corregir
    funcionalidad de buscar archivos, ya que usaba el nativo de snacks en
    lugar de telecope

--- nvim/.config/nvim/lua/core/plugins/snacks.lua
+++ nvim/.config/nvim/lua/core/plugins/snacks.lua
@@ -28,13 +28,22 @@ return {
         bigfile = { enabled = true },
         dashboard = {
             enabled = true,
+            preset = {
+                -- Sobrescribir los keymaps por defecto del dashboard
+                keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":Telescope fd" },
+                { icon = "󰉓", key = "y", desc = "Files with Yazi", action = ":Yazi" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.picker.recent()" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.picker.files({ cwd = vim.fn.stdpath('config') })" },
+                { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                { icon = "󰣇", key = "m", desc = "Mason", action = ":Mason" },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
             sections = {
                 { section = "header" },
-                {
-                    icon = "", title = "Keymaps", section = "keys", indent = 2, padding = 1,
-                    { icon = "󰣇", desc = "Mason", action = ":Mason", key = "m" },
-                    { icon = "󰉓", desc = "Files with Yazi", action = ":Yazi", key = "y" },
-                },
+                { section = "keys", indent = 2, padding = 1 },
                 { section = "startup" },
             }
         },
